e182582046906742fc7090c3740c0697f97700c7
JOHN ZEN <JOHN ZEN>
2017-02-15 14:26:32 +0800

    modify

--- server/lib.js
+++ server/lib.js
@@ -1,26 +1,25 @@
 var sql = require("./mysqlQuery");
 
-async function getData(request) {
-    var postData = "";
-    request.addListener("data", function (data) {
-        postData += data;
-    });
+module.exports = {
+    async getData(request) {
+        var postData = "";
+        request.addListener("data", function (data) {
+            postData += data;
+        });
 
-    return await request.addListener("end", function () {
-        var query = JSON.parse(postData);
-        return query;
-    });
-}
+        return await request.addListener("end", function () {
+            var query = JSON.parse(postData);
+            return query;
+        });
+    },
 
-function render(res) {
-    if (res.serverStatus == 1) {
-        res.write(JSON.stringify(res.okMsg));
-        res.end();
-    } else {
-        res.write(JSON.stringify(res.errMsg));
-        res.end();
+    render(res) {
+        if (res.serverStatus == 1) {
+            res.write(JSON.stringify(res.okMsg));
+            res.end();
+        } else {
+            res.write(JSON.stringify(res.errMsg));
+            res.end();
+        }
     }
 }
-
-exports.getData = getData;
-exports.render = render;
